dcbe2edbca9d73c912db4694c9d80d965c565374
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-04-24 19:58:15 +0300

    added the show-options function

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3.8
-
 from user import User
 from credentials import Credentials
 
-def create_user(first_name,last_name,username,password):
-    return User(first_name,last_name,username,password)
+
+def create_user(first_name, last_name, username, password):
+    """Function to create a new user"""
+    return User(first_name, last_name, username, password)
+
+def show_options():
+    print("Please choose an option to continue: ")
+    print("1. Create Account\n2. Login\n3. Display Accounts\n4. Exit")
+
+
+if __name__ == '__main__':
+
+    main()
