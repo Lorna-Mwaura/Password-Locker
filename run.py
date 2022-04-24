fe0ee93c08bba75584c2b204a8eca1e39a270ac4
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-04-24 20:25:25 +0300

    added the user_login function

--- run.py
+++ run.py
@@ -11,7 +11,30 @@ def show_options():
     print("Please choose an option to continue: ")
     print("1. Create Account\n2. Login\n3. Display Accounts\n4. Exit")
 
+def create_account_option():
+    print("Please provide the following details to create an account")
+    print('*' * 70)
 
-if __name__ == '__main__':
+    print("Enter your first name .....")
+    first_name = input()
+    print("Enter your last name .....")
+    last_name = input()
+    print("Enter your username .....")
+    username = input()
+    print("Enter password .....")
+    user_password = input()
 
-    main()
+    user = create_user(first_name, last_name, username, user_password)
+
+    user.save_user()
+
+    return user
+
+def user_login():
+    print("Enter your details to login")
+    print("Enter your username .....")
+    username = input()
+    print("Enter password .....")
+    user_password = input()
+
+    return User.login(username, user_password)
