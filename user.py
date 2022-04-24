d99df6cf8985007de402300a43911c435dc45183
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-04-24 16:44:41 +0300

    added the save user method

--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 class User:
 
+    
     def __init__(self, first_name, last_name, username, password):
         """user constructor"""
         self.first_name = first_name
@@ -8,3 +9,8 @@ class User:
         self.password = password
 
     user_accounts = []
+
+    def save_user(self):
+        """Append user to the user accounts lists"""
+        User.user_accounts.append(self)
+
