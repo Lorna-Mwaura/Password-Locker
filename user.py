e9799e4ef69d91e27639cb556a3342ef504e504a
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-04-24 17:26:12 +0300

    added the user_test file

--- user.py
+++ user.py
@@ -1,6 +1,5 @@
 class User:
 
-
     def __init__(self, first_name, last_name, username, password):
         """user constructor"""
         self.first_name = first_name
@@ -10,10 +9,26 @@ class User:
 
     user_accounts = []
 
-    def save_user(self):
-        """Append user to the user accounts lists"""
-        User.user_accounts.append(self)
-
     def delete_user(self):
         """function to delete user account"""
         User.user_accounts.remove(self)
+    
+    @classmethod
+    def login(cls, username):
+        """find user using username"""
+
+        for user in cls.user_accounts:
+            if user.username == username and user.password == password:
+                return True
+            return False
+
+    @classmethod
+    def find_user(cls,username):
+        """find user using username"""
+        for user in cls.user_accounts:
+            if user.username == username:
+                return user
+
+    def save_user(self):
+        """Append user to the user accounts lists"""
+        User.user_accounts.append(self)
--- user_test.py
+++ user_test.py
@@ -1,24 +1,21 @@
 import unittest
 from user import User
 
+
 class TestUser(unittest.TestCase):
+      
     def setUp(self):
         """setup method that runs before each test case"""
-        self.user = User("lorna","mwaura","lorna-mwaura","password")
+        self.user = User("lorna", "mwaura", "lorna-mwaura", "password")
         User.user_accounts = []
-    
+
     def test_user_created(self):
-        """Method that test if the constructor works"""
+        """Method that tests if the constructor works"""
         self.assertEqual(self.user.first_name, "lorna")
         self.assertEqual(self.user.last_name, "mwaura")
         self.assertEqual(self.user.username, "lorna-mwaura")
         self.assertEqual(self.user.password, "password")
 
-    def test_user_saved(self):
-        """Test the dave user method"""
-        self.user.save_user()
-        self.assertEqual(len(User.display_user_accounts()), 1)
 
-
-if __name__ == "__main__":
-    unittest.main()
+    if __name__ == "__main__":
+        unittest.main()
