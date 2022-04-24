0901e49bf4c906ba0a4e028c3d16aed66f8b3930
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-04-24 17:02:49 +0300

    added the test method to test the constructor

---
+++ user-test.py
@@ -0,0 +1,18 @@
+import unittest
+from user import User
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        """setup method that runs before each test case"""
+        self.user = User("lorna","mwaura","lorna-mwaura","password")
+        User.user_accounts = []
+    
+    def test_user_created(self):
+        """Method that test if the constructor works"""
+        self.assertEqual(self.user.first_name, "lorna")
+        self.assertEqual(self.user.last_name, "mwaura")
+        self.assertEqual(self.user.username, "lorna-mwaura")
+        self.assertEqual(self.user.password, "password")
+
+if __name__ == "__main__":
+    unittest.main()
