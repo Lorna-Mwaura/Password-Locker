a50b57715c888d74d8d1a13142b739f2b8a53161
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-04-24 16:35:00 +0300

    added the user constructor

---
+++ user.py
@@ -0,0 +1,10 @@
+class User:
+
+    def __init__(self, first_name, last_name, username, password):
+        """user constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+
+    user_accounts = []
